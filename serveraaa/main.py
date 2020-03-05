b434c57fbf2ee23505945f6fcc86e9ea0496ac82
MonitorPC <48449929+MonitorPC@users.noreply.github.com>
2020-03-05 10:16:57 +0300

    Update main.py

--- serveraaa/main.py
+++ serveraaa/main.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from data import db_session
+from data import users
+from users import User
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
@@ -7,6 +9,43 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 def main():
     db_session.global_init("db/blogs.sqlite")
+    user = User()
+    user.surname = 'Scott'
+    user.name = 'Ridley'
+    user.age = 21
+    user.position = 'captain'
+    user.speciality = 'research engineer'
+    user.address = 'module_1'
+    user.email = 'scott_chief@mars.org'
+
+    session = db_session.create_session()
+    session.add(user)
+    session.commit()
+
+    user.surname = 'Riple'
+    user.name = 'Alen'
+    user.age = 27
+    user.position = 'engineer'
+    user.speciality = 'engineer'
+    user.address = 'module_7'
+    user.email = 'Ailien@mars.org'
+
+    session = db_session.create_session()
+    session.add(user)
+    session.commit()
+
+    user.surname = 'Busurman'
+    user.name = 'Raile'
+    user.age = 18
+    user.position = 'doctor'
+    user.speciality = 'surgeon'
+    user.address = 'module_3'
+    user.email = 'Chumnoy_doctor@mars.org'
+
+    session = db_session.create_session()
+    session.add(user)
+    session.commit()
+
     app.run()
 
 
